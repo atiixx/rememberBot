637939a1dd90a2240177f86983675f90d3b46667
j.hauenstein <2022265@stud.hs-mannheim.de>
2021-12-10 21:41:38 +0100

    remember führt jetzt die Methode send_remember aus bot aus. send_remember eingerichtet

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ import os
 import datetime
 import pickle
 import time
-import remember
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -23,24 +22,21 @@ assignements = list()
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
+FIRSTREMINDER_DAYS = 5
+SECONDREMINDER_DAYS = 2
+LASTREMINDER_DAYS = 1
 
 bot = commands.Bot(command_prefix='!')
-test_channel = bot.get_channel(918127437068533770)
 
 @bot.command(name='neu', help='Legt ein neues Assignement an !neu <TT-MM-JJJJ> <HH:MM> <Text>')
 async def add_new(ctx, date_text, time_text, *texte):
     day, month, year = map(int, date_text.split('-'))
     hour, minute = map(int, time_text.split(':'))
-    date_and_time = datetime.datetime(year, month, day, hour, minute).strftime("%d-%m-%Y %H:%M") + "Uhr"
+    date_and_time = datetime.datetime(year, month, day, hour, minute)
     text = ''
     for worte in texte:
         text += worte + ' '
-    
-  
     await load_list() 
-
-
     items = [text.strip(), date_and_time]
     assignements.append(items)
     await save_list()
@@ -48,17 +44,17 @@ async def add_new(ctx, date_text, time_text, *texte):
 @bot.command(name='show', help='Zeigt alle Asses')
 async def show_ass(ctx):
     await load_list() 
-    
+    test_channel = bot.get_channel(918127437068533770)
     assignementString = ''
     counter = 1
     for i in assignements:
-        assignementString += f'{counter}: \'{i[0]}\' ist fällig bis zum {i[1]} \n'
+        assignementString += f'{counter} - \'{i[0]}\' ist fällig bis zum {i[1].strftime("%d-%m-%Y %H:%M")} Uhr \n'
         counter += 1
     await test_channel.send(assignementString)
     await test_channel.send("Now we wait to see if checktime works..")
     time.sleep(2)
-    await remember.check_time()
-    
+    await send_remember()
+
 
 @bot.command(name='del', help='Löscht bestimmtes Assignement !del <index> - Um den Index zu sehen !show')
 async def delete_ass(ctx, index):
@@ -66,6 +62,23 @@ async def delete_ass(ctx, index):
     await load_list()
     del assignements[int(index)-1]
     await save_list()
+
+async def send_remember():
+    await load_list()
+    test_channel = bot.get_channel(918127437068533770)
+    now = datetime.datetime.now()
+    for i in range(len(assignements)):
+        if assignements[i][1] < now:
+            await load_list()
+            del assignements[i]
+            await save_list()
+        elif now > assignements[i][1] - datetime.timedelta(days = FIRSTREMINDER_DAYS) and now < assignements[i][1] - datetime.timedelta(days = SECONDREMINDER_DAYS):
+            await test_channel.send(f'Abgabe in 5 Tagen fällig: "{assignements[i][0]}"')
+        elif now > assignements[i][1] - datetime.timedelta(days = SECONDREMINDER_DAYS) and now < assignements[i][1] - datetime.timedelta(days = LASTREMINDER_DAYS):
+            await test_channel.send(f'Abgabe in 2 Tagen fällig: "{assignements[i][0]}"') 
+        elif now > assignements[i][1]- datetime.timedelta(days = LASTREMINDER_DAYS):
+            await test_channel.send(f'!!Abgabe morgen fällig: "{assignements[i][0]}"!!')    
+
 
 async def save_list():
     pickle.dump(assignements, open('assignement_list.obj', 'wb'))
