25ce98f1a17add8530e32b61289c1bd258606f9c
j.hauenstein <2022265@stud.hs-mannheim.de>
2021-12-12 15:28:05 +0100

    changed channel

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@
 #
 #Bot um Erinnerung für die HS einzustellen
 #author: Jonas Hauenstein
-#Date last edited: 10.12.2021
-#To-Do to prod.: 
-#Automatische Reminder 5 Tage vorher + 1 Tag vorher (evtl cron?)
-#Error handling
+#Date last edited: 12.12.2021
+#To-Do: 
 #Mehrere Assignements löschen
 
 
@@ -36,11 +34,11 @@ async def add_new(ctx, date_text, time_text, *texte):
         hour, minute = map(int, time_text.split(':'))
 
         if (day > 31 or day < 1 or month > 12 or month < 1 or year > 2030 or year < datetime.datetime.now().year):
-            await bot.get_channel(918127437068533770).send("Ungültige Datumsangabe")
+            await bot.get_channel(919595793646776371).send("Ungültige Datumsangabe")
             exit
 
         if (hour < 0 or hour > 24 or minute < 0 or minute > 59):
-            await bot.get_channel(918127437068533770).send("Ungültige Zeitangabe")
+            await bot.get_channel(919595793646776371).send("Ungültige Zeitangabe")
             exit
 
         date_and_time = datetime.datetime(year, month, day, hour, minute)
@@ -52,14 +50,14 @@ async def add_new(ctx, date_text, time_text, *texte):
         assignements.append(items)
         await save_list()
     except (ValueError) as err:
-        await bot.get_channel(918127437068533770).send("Bitte folgende Syntax nutzen: \n!neu <TT-MM-JJJJ> <HH:MM> <Text>")
+        await bot.get_channel(919595793646776371).send("Bitte folgende Syntax nutzen: \n!neu <TT-MM-JJJJ> <HH:MM> <Text>")
 
 
 @bot.command(name='show', help='Zeigt alle Asses')
 async def show_ass(ctx):
     try:
         await load_list() 
-        test_channel = bot.get_channel(918127437068533770)
+        test_channel = bot.get_channel(919595793646776371)
         assignementString = ''
         counter = 1
         for i in assignements:
@@ -70,7 +68,7 @@ async def show_ass(ctx):
         time.sleep(2)
         await send_remember()
     except:
-        await bot.get_channel(918127437068533770).send("Alles erledigt! Keine Assignements übrig :)")
+        await bot.get_channel(919595793646776371).send("Alles erledigt! Keine Assignements übrig :)")
 
 
 @bot.command(name='del', help='Löscht bestimmtes Assignement !del <index> - Um den Index zu sehen !show')
@@ -81,7 +79,7 @@ async def delete_ass(ctx, index):
         del assignements[int(index)-1]
         await save_list()
     except:
-        await bot.get_channel(918127437068533770).send("Assignement nicht vorhanden")
+        await bot.get_channel(919595793646776371).send("Assignement nicht vorhanden")
 
 
 @bot.event
@@ -97,7 +95,7 @@ async def on_command_error(ctx, error):
 
 async def send_remember():
     await load_list()
-    test_channel = bot.get_channel(918127437068533770)
+    test_channel = bot.get_channel(919595793646776371)
     now = datetime.datetime.now()
     for i in range(len(assignements)):
         if assignements[i][1] < now:
