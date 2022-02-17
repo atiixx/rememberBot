266853104301a71d155573e893f0dd2d97868a54
j.hauenstein <2022265@stud.hs-mannheim.de>
2022-02-17 20:57:14 +0100

    funktionsfähig meiner Meinung nach

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 #ReminderBot
-#
+
 #Bot um Erinnerung für die HS einzustellen
 #author: Jonas Hauenstein
 #Date last edited: 12.12.2021
 #To-Do: 
 #Mehrere Assignements löschen
-
+#Feedback wenn erfolgreich
+#check if sender is bot not working
 
 import os
-import datetime
 import pickle
-import time
 
+from datetime import datetime, timedelta
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -20,103 +20,90 @@ assignements = list()
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+TEST_CHANNEL = 918127437068533770
+PROD_CHANNEL = 919595793646776371
 FIRSTREMINDER_DAYS = 5
 SECONDREMINDER_DAYS = 2
 LASTREMINDER_DAYS = 1
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='neu', help='Legt ein neues Assignement an\n!neu <TT-MM-JJJJ> <HH:MM> <Text>')
-async def add_new(ctx, date_text, time_text, *texte):
-    """Added ein neues Assignement zur Liste"""
-
-    if date_text.author.bot:
-        return
-
+@bot.command(name='neu', help='Legt ein neues Assignement an\n!neu <TT-MM-JJ> <HH:MM> <Text>')
+async def add_ass(ctx, date_text, time_text, *texte):
     try:
-        day, month, year = map(int, date_text.split('-'))
-        hour, minute = map(int, time_text.split(':'))
+        date_time_string = date_text + " " + time_text
 
-        if (day > 31 or day < 1 or month > 12 or month < 1 or year > 2030 or year < datetime.datetime.now().year):
-            await bot.get_channel(919595793646776371).send("Ungültige Datumsangabe")
-            exit
+        date_and_time = datetime.strptime(date_time_string, '%d-%m-%y %H:%M')
 
-        if (hour < 0 or hour > 24 or minute < 0 or minute > 59):
-            await bot.get_channel(919595793646776371).send("Ungültige Zeitangabe")
-            exit
-
-        date_and_time = datetime.datetime(year, month, day, hour, minute)
         text = ''
+
         for worte in texte:
             text += worte + ' '
-        await load_list() 
+
+        if assignements:
+            await load_list() 
+
         items = [text.strip(), date_and_time]
         assignements.append(items)
         await save_list()
-    except (ValueError) as err:
-        await bot.get_channel(919595793646776371).send("Bitte folgende Syntax nutzen: \n!neu <TT-MM-JJJJ> <HH:MM> <Text>")
+        await send_remember()
+        await bot.get_channel(TEST_CHANNEL).send("Ass hinzugefügt")
+    except ValueError:
+        await bot.get_channel(TEST_CHANNEL).send("Datum oder Datumsformat ungültig. Format: dd-mm-yy hh:mm")
+
 
 
 @bot.command(name='show', help='Zeigt alle Asses')
 async def show_ass(ctx):
-
-    
-
-    try:
-        await load_list() 
-        test_channel = bot.get_channel(919595793646776371)
+        if(assignements):
+            await load_list()
+        else:
+             await bot.get_channel(TEST_CHANNEL).send("Alles erledigt :)")
+             return ""
         assignementString = ''
         counter = 1
         for i in assignements:
-            assignementString += f'{counter} - \'{i[0]}\' ist fällig bis zum {i[1].strftime("%d-%m-%Y %H:%M")} Uhr \n'
+            assignementString += f'{counter} - \'{i[0]}\' ist fällig bis zum {i[1].strftime("%d-%m-%Y %H:%M")} Uhr\n'
             counter += 1
-        await test_channel.send(assignementString)
-
-    except:
-        await bot.get_channel(919595793646776371).send("Alles erledigt! Keine Assignements übrig :)")
+        await bot.get_channel(TEST_CHANNEL).send(assignementString)
 
 
 @bot.command(name='del', help='Löscht bestimmtes Assignement !del <index> - Um den Index zu sehen !show')
 async def delete_ass(ctx, index):
     #TO-DO: Mehrere löschen zB !del 2-4 (löscht 2,3 und 4)
 
-    if index.author.bot:
-        return
-
+   # if index.author.bot:
+   #return
     try:
         await load_list()
+        await bot.get_channel(TEST_CHANNEL).send(f"Assignement \"{assignements[int(index)-1][0]}\" erfolgreich gelöscht.")
         del assignements[int(index)-1]
         await save_list()
-    except:
-        await bot.get_channel(919595793646776371).send("Assignement nicht vorhanden")
+    except IndexError:
+        await bot.get_channel(TEST_CHANNEL).send(f"Es gibt kein Assignement mit der Nummer {index}")
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    send_help = (commands.MissingRequiredArgument, commands.BadArgument, commands.TooManyArguments, commands.UserInputError)
-
-    if isinstance(error, commands.CommandNotFound):  # fails silently
-        pass
-
-    elif isinstance(error, send_help):
-        await ctx.send("Bitte richtige Syntax nutzen! \n!help oder !help <command> für mehr Informationen")
 
 
 async def send_remember():
-    await load_list()
-    test_channel = bot.get_channel(919595793646776371)
-    now = datetime.datetime.now()
+    if assignements:
+        await load_list()
+    else:
+        print("Keine Reminder geschickt.")
+        return ""
+    channel = bot.get_channel(TEST_CHANNEL)
+    now = datetime.today()
     for i in range(len(assignements)):
         if assignements[i][1] < now:
             await load_list()
+            await channel.send(f'\"{assignements[i][0]}\" vorbei. Wird gelöscht...')
             del assignements[i]
             await save_list()
-        elif now > assignements[i][1] - datetime.timedelta(days = FIRSTREMINDER_DAYS) and now < assignements[i][1] - datetime.timedelta(days = SECONDREMINDER_DAYS):
-            await test_channel.send(f'Abgabe in 5 Tagen fällig: "{assignements[i][0]}"')
-        elif now > assignements[i][1] - datetime.timedelta(days = SECONDREMINDER_DAYS) and now < assignements[i][1] - datetime.timedelta(days = LASTREMINDER_DAYS):
-            await test_channel.send(f'Abgabe in 2 Tagen fällig: "{assignements[i][0]}"') 
-        elif now > assignements[i][1]- datetime.timedelta(days = LASTREMINDER_DAYS):
-            await test_channel.send(f'!!Abgabe morgen fällig: "{assignements[i][0]}"!!')    
+        elif now > assignements[i][1] - timedelta(days=5) and now < assignements[i][1] - timedelta(days = 2):
+            await channel.send(f'\"{assignements[i][0]}\" in unter 5 Tagen fällig!!')
+        elif now > assignements[i][1] - timedelta(days=2) and now < assignements[i][1] - timedelta(days = 1):
+            await channel.send(f'\"{assignements[i][0]}\" in unter 2 Tagen fällig!!') 
+        elif now >= assignements[i][1]- timedelta(days=1):
+            await channel.send(f'\"{assignements[i][0]}\" !!Abgabe morgen fällig!!')    
 
 
 async def save_list():
